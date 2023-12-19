9dfbf6249eced844f02be273a331174c717e2ac4
Tuki1004 <tuki10041999@gmail.com>
2023-12-19 16:45:13 +0300

    Добавьте файлы проекта.

---
+++ GamrMatching/Form1.Designer.cs
@@ -0,0 +1,124 @@
+﻿namespace GamrMatching
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lGameName = new System.Windows.Forms.Label();
+            this.lName = new System.Windows.Forms.Label();
+            this.tbName = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.bEasy = new System.Windows.Forms.Button();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lGameName
+            // 
+            this.lGameName.AutoSize = true;
+            this.lGameName.Font = new System.Drawing.Font("Segoe Script", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lGameName.ForeColor = System.Drawing.Color.MediumVioletRed;
+            this.lGameName.Location = new System.Drawing.Point(32, 20);
+            this.lGameName.Name = "lGameName";
+            this.lGameName.Size = new System.Drawing.Size(363, 64);
+            this.lGameName.TabIndex = 0;
+            this.lGameName.Text = "Matching Game";
+            // 
+            // lName
+            // 
+            this.lName.AutoSize = true;
+            this.lName.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.lName.Location = new System.Drawing.Point(30, 95);
+            this.lName.Name = "lName";
+            this.lName.Size = new System.Drawing.Size(48, 17);
+            this.lName.TabIndex = 1;
+            this.lName.Text = "Player";
+            // 
+            // tbName
+            // 
+            this.tbName.Location = new System.Drawing.Point(33, 115);
+            this.tbName.Name = "tbName";
+            this.tbName.Size = new System.Drawing.Size(230, 20);
+            this.tbName.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(30, 160);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(89, 17);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Choose level";
+            // 
+            // bEasy
+            // 
+            this.bEasy.Location = new System.Drawing.Point(0, 14);
+            this.bEasy.Name = "bEasy";
+            this.bEasy.Size = new System.Drawing.Size(154, 36);
+            this.bEasy.TabIndex = 0;
+            this.bEasy.Text = "Easy";
+            this.bEasy.UseVisualStyleBackColor = true;
+            this.bEasy.Click += new System.EventHandler(this.bEasy_Click);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.bEasy);
+            this.panel1.Location = new System.Drawing.Point(32, 180);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(185, 139);
+            this.panel1.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(466, 425);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tbName);
+            this.Controls.Add(this.lName);
+            this.Controls.Add(this.lGameName);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lGameName;
+        private System.Windows.Forms.Label lName;
+        private System.Windows.Forms.TextBox tbName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button bEasy;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ GamrMatching/Form2.cs
@@ -0,0 +1,185 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace GamrMatching
+{
+    public partial class Form2 : Form
+    {
+
+        Random random = new Random();
+
+        // Лист со списком значений карточек
+        List<string> icons = new List<string>()
+        {
+            "a", "a", "b", "b", "c", "c", "d", "d", "g", "g", "^", "^", "h", "h", "i", "i"
+        };
+
+        int Count = 0;
+
+        // Метка укаывающая на первое нажатие
+        Label firstClicked = null;
+
+        // Метка указывающая на второе нажатие
+        Label secondClicked = null;
+
+        public Form2()
+        {
+            InitializeComponent();
+
+            AssignIconsToSquares();
+        }
+
+        // Увеличение счетчика ходов и обновление метки в форме
+        private void IncrementMovesCount()
+        {
+            Count++;
+            lCount.Text = Count.ToString();
+        }
+
+        // Обработчик клика по иконке
+        private void label1_Click(object sender, EventArgs e)
+        {
+            
+            if (timer1.Enabled == true)
+            {
+                return;
+            }
+
+            Label clickedLabel = sender as Label;
+            if (clickedLabel != null)
+            {
+                
+                if(clickedLabel.ForeColor == Color.FromArgb(128, 128, 255))
+                {
+                    return;
+                }
+
+                if (firstClicked == null)
+                {
+                    firstClicked = clickedLabel;
+                    clickedLabel.ForeColor = Color.FromArgb(128, 128, 255);
+                    return;
+                }
+
+                secondClicked = clickedLabel;
+                secondClicked.ForeColor = Color.FromArgb(128, 128, 255);
+
+                CheckForWinner();
+                IncrementMovesCount();
+
+                if (firstClicked.Text == secondClicked.Text)
+                {
+                    firstClicked = null;
+                    secondClicked = null;
+                    return;
+                }
+
+                timer1.Start();
+            }
+        }
+
+        // Проверка неоткрытых иконок
+        private void CheckForWinner()
+        {
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconLabel = control as Label;
+                if (iconLabel != null)
+                {
+                    if (iconLabel.ForeColor == iconLabel.BackColor)
+                    {
+                        return;
+                    }
+                }
+            }
+                MessageBox.Show("You win!!! Count: " + Count);
+                Close();
+        }
+
+        // Назначение каждому квадрату случайный элемент из списка 
+        private void AssignIconsToSquares()
+        {
+            foreach (Control control in tableLayoutPanel1.Controls)
+            {
+                Label iconLabel = control as Label;
+                if (iconLabel != null)
+                {
+                    int randomNumber = random.Next(icons.Count);
+                    iconLabel.Text = icons[randomNumber];
+                    iconLabel.ForeColor = iconLabel.BackColor;
+                    icons.RemoveAt(randomNumber);
+                }
+            }
+        }
+
+        // Запускается когда игрок нажимает две не совпадающие иконки, затем отсчитывается 3 секунды и выключается.
+        // после обе иконки скрываются
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            timer1.Stop();
+
+            firstClicked.ForeColor = firstClicked.BackColor;
+            secondClicked.ForeColor = secondClicked.BackColor;
+
+            firstClicked = null;
+            secondClicked = null;
+        }
+
+        System.Timers.Timer Timer;
+        int m, s;
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            Timer = new System.Timers.Timer();
+            Timer.Interval = 1000; // 1s
+            Timer.Elapsed += OnTimeEvent;
+            Timer.Start();
+
+            tbTime.ReadOnly = true;
+        }
+
+        private void Form2_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            Timer.Stop();
+            Timer.Dispose();
+        }
+
+        // подсчет времени
+        private void OnTimeEvent(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            // Проверяем что форма еще не закрыта
+            if (!IsDisposed)
+            {
+                Invoke((MethodInvoker)(() =>
+                {
+                    s = s + 1;
+                    if (s == 60)
+                    {
+                        s = 0;
+                        m = m + 1;
+                    }
+                    tbTime.Text = string.Format("{0}:{1}", m.ToString().PadLeft(2, '0'), s.ToString().PadLeft(2, '0'));
+
+                    if (m == 2)
+                    {
+                        Timer.Stop();
+                        MessageBox.Show("You lose!");
+                    }
+
+                }));
+                
+            }
+            string Result = tbTime.Text;
+            int minut = m;
+        }
+
+
+    }
+}
